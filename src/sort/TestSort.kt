ae6f3fc9067131e910cb1ddff307abdf5d003d2a
huangshouguo <huangshouguo@lvjinsuo.com>
2017-07-10 23:30:40 +0800

    add generateRandomUniqueData function

--- src/sort/TestSort.kt
+++ src/sort/TestSort.kt
@@ -1,5 +1,7 @@
 package sort
 
+import java.util.*
+
 private fun show(data: Array<Int>?): String {
     var result: String = ""
 
@@ -17,13 +19,29 @@ private fun test(dataArray: Array<Int>?, sort: (dataArray: Array<Int>?) -> Array
     println("sort after: ${show(sort(dataArray))}")
 }
 
+private fun generateRandomUniqueData(total: Int): Array<Int> {
+    val data: Array<Int> = Array(total, { i -> i })
+    val output: IntArray = IntArray(total)
+
+    var end = total - 1
+
+    val random = Random()
+    for (i in 0 until total) {
+        val num = random.nextInt(end + 1)
+        output[i] = data[num]
+        data[num] = data[end]
+        end--
+    }
+
+    return output.toTypedArray()
+}
+
 fun main(args: Array<String>) {
-    val dataArray = arrayOf(9, 7, 6, 2, 3, 5, 8, 1, 4, 10, 0)
+    val dataLength = 20
 
-//    test(dataArray, ::bubbleSort)
-    test(dataArray, ::quickSort)
+    println("\nBubbleSort:")
+    test(generateRandomUniqueData(dataLength), ::bubbleSort)
 
-//    test(dataArray) {
-//        bubbleSort(dataArray)
-//    }
+    println("\nQuickSort:")
+    test(generateRandomUniqueData(dataLength), ::quickSort)
 }
